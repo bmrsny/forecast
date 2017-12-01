1d57b86a4c3c6e80c22267d1bd875a89205c4321
Brandon Mrsny <bmrsny@C02V91FU.local>
2017-12-01 11:07:03 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forecast
+Have a working installation of Elixir 1.5.2
+Tutorial to build this application can be found in priv/tutorial.md
---
+++ lib/forecast.ex
@@ -0,0 +1,18 @@
+defmodule Forecast do
+  @moduledoc """
+  Documentation for Forecast.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Forecast.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
